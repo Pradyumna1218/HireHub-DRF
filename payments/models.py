e737164ca36db2a9bf6eb1a7ef3efe4b14ed1e55
Pradyumna <109955188+Pradyumna1218@users.noreply.github.com>
2025-04-30 12:18:28 +0545

    Models For Order And Payment

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from users.models import Client, User
+from services.models import Service
+from django.utils import timezone
 
-# Create your models here.
+
+class Order(models.Model):
+
+    status_choices = [
+        ("Pending", "Pending"),
+        ("In Progress", "In Progress"),
+        ("Completed", "Completed"),
+    ]
+
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name= 'orders')
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name= 'orders')
+    
+    order_date = models.DateTimeField(default=timezone.now)
+    delivery_date = models.DateTimeField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50, choices=status_choices, default="Pending")
+
+class Payment(models.Model):
+    status_choices = [
+        ("Pending", "Pending"),
+        ("In Progress", "In Progress"),
+        ("Completed", "Completed"),
+    ]
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="payments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments")
+
+    status = models.CharField(max_length=50, choices=status_choices, default="Pending")
+    payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(default=timezone.now)
+    
