e5180e11769088cea627a6f62bd4d543044628ec
Pradyumna <109955188+Pradyumna1218@users.noreply.github.com>
2025-05-12 09:54:19 +0545

    Celery Beat

--- hirehub_project/celery.py
+++ hirehub_project/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hirehub_project.settings')
 
@@ -13,3 +14,9 @@ app.conf.update(timezone='Asia/Kathmandu')
 app.config_from_object(settings, namespace='CELERY')
 
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    'delete-expired-password-reset-tokens': {
+        'task': 'users.tasks.delete_expired_tokens',
+        'schedule': crontab(minute='*/10'),  
+    },
+}
--- hirehub_project/settings.py
+++ hirehub_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_celery_results',
     'channels',
     'communications',
+    'django_celery_beat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
